1eab5a49a5a73e20adbeedd6c51ef425e1952c9d
Stas <luchano057@gmail.com>
2018-07-04 03:13:51 +0700

    diploma 03

--- diploma/js/script.js
+++ diploma/js/script.js
@@ -45,14 +45,14 @@ let tab = document.getElementsByClassName('glazing_block'),
     tabContent = document.getElementsByClassName('row'),
     info = document.getElementsByClassName('glazing_slider')[0];
 
-function hideTabContent(a) {
-							for ( let i = a; i < tabContent.length; i++) {
-								tabContent[i].classList.remove('show');
-								tabContent[i].classList.add('hide');
-							}
+// function hideTabContent(a) {
+// 							for ( let i = a; i < tabContent.length; i++) {
+// 								tabContent[i].classList.remove('show');
+// 								tabContent[i].classList.add('hide');
+// 							}
 
-}
-					hideTabContent(1);
+// }
+// 					hideTabContent(1);
 
 	function showTabContent(b) {
 						if(tabContent[b].classList.contains('glazing')) {
@@ -78,7 +78,62 @@ info.addEventListener('click', function(event) {
     });
 
 
+
+
+
+
+let deadline = '2018-07-04';
+
+     function getTimeRemaining(endtime) {
+     	  let t = Date.parse(endtime) - Date.parse(new Date()),
+     	  seconds = Math.floor( (t/1000) % 60 ),
+     	  minutes = Math.floor( (t/1000/60) % 60 ),
+     	  hours = Math.floor( (t/(1000*60*60)) ),
+     	  days = Math.floor(t/(1000 * 60 * 60 * 24));
+     	  t = +t;
+
+     	  return {
+     	  	'total': t,
+     	  	'days': days,
+     	  	'hours': hours,
+     	  	'minutes': minutes,
+     	  	'seconds': seconds
+     	  }
+
+     };
+
+     let timeInterval;
+
+     function setClock(id, endtime) {
+     	let timer = document.getElementById(id),
+     					days = timer.querySelector('.days')
+     					hours = timer.querySelector('.hours'),
+     					minutes = timer.querySelector('.minutes'),
+     					seconds = timer.querySelector('.seconds');
+
+     					function updateClock() {
+     						let t = getTimeRemaining(endtime);
+     						days.innerHTML = ("0" + t.days).slice(-2);
+     						hours.innerHTML = ("0" + t.hours).slice(-2);
+     						minutes.innerHTML = ("0" + t.minutes).slice(-2);
+     						seconds.innerHTML = ("0" + t.seconds).slice(-2);
+
+     						if (t.total <= 0) {
+     									clearInterval(timeInterval);
+     									days.textContent = "00";
+     									hours.textContent = "00";
+     						   minutes.textContent = "00";
+     						   seconds.textContent = "00";
+
+     						}
+     					};
+
+     					updateClock();
+     					timeInterval = setInterval(updateClock, 1000);
+
+     };
+
+     setClock('timer', deadline);
+
 });
-
-
 
